614e3d52b50e35c59cb72b9dd056b51a40f571c8
Xuanson On <hello@xuansonon.com>
2016-12-23 13:09:11 +1100

    Add files via upload

--- TwelveDaysOfChristmas.java
+++ TwelveDaysOfChristmas.java
@@ -3,60 +3,18 @@ import java.io.*;
 class TwelveDaysOfChristmas {
   static String[] days = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "nineth", "tenth", "eleventh", "twelveth"};
   static String[] gifts = {"Partridge in a Pear Tree", "Turtle Doves", "French Hens", "Calling Birds", "Golden Rings", "Geese a Laying", "Swans a Swimming", "Maids a Milking", "Ladies Dancing", "Lords a Leaping", "Pipers Piping", "Drummers Drumming"};
-
-  public static String intToString(int aNumber) {
-    String valueOfInt = null;
-    switch(aNumber) {
-      case 1:
-        valueOfInt = "a";
-        break;
-      case 2:
-        valueOfInt = "two";
-        break;
-      case 3:
-        valueOfInt = "three";
-        break;
-      case 4:
-        valueOfInt = "four";
-        break;
-      case 5:
-        valueOfInt = "five";
-        break;
-      case 6:
-        valueOfInt = "six";
-        break;
-      case 7:
-        valueOfInt = "seven";
-        break;
-      case 8:
-        valueOfInt = "eigth";
-        break;
-      case 9:
-        valueOfInt = "nine";
-        break;
-      case 10:
-        valueOfInt = "ten";
-        break;
-      case 11:
-        valueOfInt = "eleven";
-        break;
-      case 12:
-        valueOfInt = "twleve";
-        break;
-      default:
-        break;
-    }
-    return valueOfInt;
-  }
+  static String[] plural = {"a", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"};
 
   public static void main(String[] args) {
     for(int i = 0; i < 12; i++) {
       System.out.println("On the " + days[i] + " day of Christmas\nMy true love gave to me:");
       for(int j = i; j >= 0; j--) {
+        String giftLine = "";
         if((i > 0) && (j == 0)) {
-          System.out.print("and ");
+          giftLine += "and ";
         }
-        System.out.println(intToString(j + 1) + " " + gifts[j]);
+        giftLine += (plural[j] + " " + gifts[j]);
+        System.out.println(giftLine.substring(0,1).toUpperCase() + giftLine.substring(1));
       }
       System.out.println("");
     }
